5c23edb098b63bf3fa04dd59aa3afc56464d7380
SameGevDudios <magomed.chapaev1@mail.ru>
2025-05-11 19:26:35 +0300

    Add method for mapping pairs from file

--- Task1.py
+++ Task1.py
@@ -23,3 +23,10 @@ def count_valid_pairs(ratings, k):
             left += 1
     return count
 
+
+# Read file
+def process_file(filename):
+    with open(filename, 'r') as f:
+        n, k = map(int, f.readline().split())
+        ratings = [int(f.readline()) for _ in range(n)]
+    return count_valid_pairs(ratings, k)
